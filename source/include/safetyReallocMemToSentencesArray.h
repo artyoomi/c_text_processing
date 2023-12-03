d95eb6c23dec0e5398016be4a1bf4aa194b09cca
artyoomi <zuodleq@gmail.com>
2023-12-03 19:18:47 +0300

    Commit before split to files

---
+++ source/include/safetyReallocMemToSentencesArray.h
@@ -0,0 +1,1 @@
+void safetyReallocMemToSentencesArray(struct Sentence*** buffer, int32_t* count_of_allocated_sentences);
---
+++ source/include/safetyReallocMemToWordsArray.h
@@ -0,0 +1,1 @@
+void safetyReallocMemToWordsArray(struct Word*** buffer, int32_t* count_of_allocated_words);
